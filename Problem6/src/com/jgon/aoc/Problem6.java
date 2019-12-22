c1191c1d9c89419efc427ae9cb4496f73e1a81f4
Jeff Gonis <jeffgonis@fastmail.com>
2019-12-22 02:31:13 -0700

    Prep for Problem 6

---
+++ Problem6/src/com/jgon/aoc/Problem6.java
@@ -0,0 +1,20 @@
+package com.jgon.aoc;
+
+import java.io.InputStream;
+
+public class Problem6 {
+		public static void main(String[] args) throws Exception {
+			Problem6 app = new Problem6();
+			InputStream input = app.getClass().getResourceAsStream("/p6input.txt");
+			System.out.println("Problem 6_1: " + problem6_1());
+			System.out.println("Problem 6_2: " + problem6_2());
+		}
+
+		public static int problem6_1()  {
+			return -1;
+		}
+
+		public static int problem6_2() {
+			return -2;
+		}
+}
---
+++ Problem6/tests/com/jgon/aoc/Problem6Test.java
@@ -0,0 +1,23 @@
+package com.jgon.aoc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Problem6Test {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void problem6_1() {
+		assertEquals(-1, Problem6.problem6_1());
+	}
+
+	@Test
+	void problem6_2() {
+		assertEquals(-2, Problem6.problem6_2());
+	}
+}
